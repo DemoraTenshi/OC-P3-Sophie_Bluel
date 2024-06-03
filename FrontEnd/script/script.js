1669be18352a8417e6c93436c9ca21e3d2facc80
DemoraTenshi <croq45@gmail.com>
2024-06-03 21:37:03 +0200

    add filterGallery Function

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,96 +1,104 @@
-console.log("ça marche!")
+const gallery = document.querySelector(".gallery");
 
 // Récupération de la gallerie et affichage des projets//
 async function getWorks() {
-    //Récupération des données//
-    await fetch("http://localhost:5678/api/works")
-        .then((response) => {
-            if (!response.ok) {
-            throw new Error("Erreur lors de la récupération des données");
-        }
-        return response.json();
-      })
-    
-      .then((data) => {
-        const gallery = document.querySelector(".gallery");
-        //Création des balises html//
-        data.forEach((item) => {
-          const projectElement = document.createElement("article");
-          projectElement.classList.add("projectElement");
-         
-          const imgElement = document.createElement("img");
-          imgElement.src = item.imageUrl;
-          imgElement.alt = item.title;
-  
-          const titleElement = document.createElement("figcaption");
-          titleElement.textContent = item.title;
+  //Récupération des données//
+  await fetch("http://localhost:5678/api/works")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur lors de la récupération des données");
+      }
+      return response.json();
+    })
+
+    .then((data) => {
+      //Création des balises html//
+      data.forEach((item) => {
+        const projectElement = document.createElement("article");
+        projectElement.classList.add("projectElement");
+        projectElement.setAttribute("data-category", item.category.name);
+
+        const imgElement = document.createElement("img");
+        imgElement.src = item.imageUrl;
+        imgElement.alt = item.title;
+
+        const titleElement = document.createElement("figcaption");
+        titleElement.textContent = item.title;
 
         //Ajout des éléments dans le DOM//
-          gallery.appendChild(projectElement);
-          projectElement.appendChild(imgElement);
-          projectElement.appendChild(titleElement);
-        });
+        gallery.appendChild(projectElement);
+        projectElement.appendChild(imgElement);
+        projectElement.appendChild(titleElement);
       });
+    });
+}
 
+getWorks();
 
-  }
-
-  getWorks();
-
-//Récupérations des catégories et affichage des boutons de filtres  
-const filtersArray = [];
-
+//Récupérations des catégories et affichage des boutons de filtres//
 async function getCategories() {
-  console.log("Appel de getCategories")
   const buttonContainer = document.querySelector(".buttons");
-    if (buttonContainer === null) {
+  if (buttonContainer === null) {
     return;
-    }
+  }
+  // Création du bouton "Tous"//
   const anyButton = document.createElement("button");
   anyButton.classList.add("filter-button");
   anyButton.setAttribute("id", "selected");
-  anyButton.textContent = ("Tous");
+  anyButton.textContent = "Tous";
   buttonContainer.appendChild(anyButton);
-  filtersArray.push(anyButton);
+  anyButton.addEventListener("click", () => {
+    (gallery.innerHTML = ""), getWorks();
+  });
 
+  //Récupération des données//
+  await fetch("http://localhost:5678/api/categories")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur lors de la récupération des données");
+      }
+      return response.json();
+    })
 
-    //Récupération des données//
-    await fetch("http://localhost:5678/api/categories")
-        .then((response) => {
-            if (!response.ok) {
-            throw new Error("Erreur lors de la récupération des données");
-        }
-      
-        return response.json();
-      })
+    .then((data) => {
+      // Création des catégories//
+      const categoriesSet = new Set();
+      data.forEach((item) => {
+        categoriesSet.add(item.name);
+      });
 
-        .then((data) => {
-          // Création des catégories//
-          const categoriesSet = new Set();
-          data.forEach((item) => {
-          categoriesSet.add(item.name);
+      //Création des filtres//
+      const categories = Array.from(categoriesSet);
+      categories.forEach((category) => {
+        const button = document.createElement("button");
+        button.textContent = category;
+        button.classList.add("filter-button");
+        //Ajout dans le DOM//
+        buttonContainer.appendChild(button);
+        //Ajout de l'eventListener//
+        button.addEventListener("click", () => {
+          filterGallery(category);
         });
-        
+      });
 
-          //Création des filtres//
-          const categories = Array.from(categoriesSet);
-          categories.forEach((category) =>{
-            const button = document.createElement("button");
-            button.textContent = category;
-            button.classList.add("filter-button");
-            buttonContainer.appendChild(button);
-            filtersArray.push(button);
-          
+      //fonction pour filtrer la galerie en fonction de la catégorie sélectionnée//
+      function filterGallery(category) {
+        // Récupération des éléments de la galerie//
+        const galleryItems = document.querySelectorAll(".projectElement");
 
-          })
+        // Parcours des éléments de la galerie//
+        galleryItems.forEach((item) => {
+          const itemCategory = item.getAttribute("data-category");
 
-        
-        })
-  
-    }
+          // Vérification si la catégorie de l'élément correspond à la catégorie sélectionnée//
+          if (category === undefined || category === itemCategory) {
+            item.style.display = "block";
+          } else {
+            item.style.display = "none";
+          }
+        });
+      }
+    });
+}
 
-    getCategories()
-
-
-
-
+getCategories();
